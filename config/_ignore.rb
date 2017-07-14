a71f436fe6ebbea52da2e55d9559e6f19adc0a61
Matthew Cataldi <mcataloe@gmail.com>
2017-07-14 11:08:28 -0500

    Redo, again!

--- config/_ignore.rb
+++
@@ -1,7 +0,0 @@
-ENV["CLIENT_ID"] = "59086680245-55aesnvlddh37qqaf7kam5a1u3pkvfni.apps.googleusercontent.com"
-ENV["CLIENT_SECRET"] = "6Tnrhrok7tYElsndUPu8kc8Y"
-ENV["PROJECT_ID"] = "mjcataldi-173616"
-ENV["AUTH_URI"] = "https://accounts.google.com/o/oauth2/auth"
-ENV["TOKEN_URI"] = "https://accounts.google.com/o/oauth2/token"
-
-# {"web":{"client_id":"59086680245-55aesnvlddh37qqaf7kam5a1u3pkvfni.apps.googleusercontent.com","project_id":"mjcataldi-173616","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"6Tnrhrok7tYElsndUPu8kc8Y","javascript_origins":["http://localhost:9393"]}}
